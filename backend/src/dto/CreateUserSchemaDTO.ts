80cdc7a2b1bbb2ce85ad1e89f6da6b668fb49d52
princeDev <davinomoises@outlook.com>
2026-04-01 19:41:04 -0400

    feat:cria validacao para criacao de user

--- backend/src/dto/CreateUserSchemaDTO.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-import { User } from "../entities/User"
-
-export const createUserSchema = z.object({
-    
-
-})
-
---
+++ backend/src/dto/userDTO/CreateUserSchemaDTO.ts
@@ -0,0 +1,22 @@
+import { z } from "zod"
+
+
+export const createUserSchema = z.object({
+    nome: z.string().trim().min(1, "Nome e obrigatorio").max(100, "Nome deve ter no maximo 100 caracteres"),
+    email: z.email().trim().toLowerCase(),
+    senha: z.string().min(6).refine((s) => /[A-Z]/.test(s), {
+        error: "A senha deve conter ao menos 1 letra maiuscula"
+    })
+    .refine((s) => /[a-z]/.test(s), {
+        error: "A senha deve conter ao menos 1 letra minuscula"
+    })
+    .refine((s) => /[^A-Za-z0-9]/.test(s), {
+        error: "A senha deve conter ao menos 1 caracter especial"
+    })
+    .refine((s) => /[0-9]/.test(s), {
+        error: "A senha deve conter ao menos 1 numero"
+    })
+
+})
+
+export type CreateUserSchemaDTO = z.infer<typeof createUserSchema>
