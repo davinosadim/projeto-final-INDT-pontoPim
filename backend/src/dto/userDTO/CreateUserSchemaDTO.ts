9f7254495ee6b5ae687d7bc329bc583f0fe4ea06
princeDev <davinomoises@outlook.com>
2026-04-01 19:46:59 -0400

    refactor:ajusta userSchema

--- backend/src/dto/userDTO/CreateUserSchemaDTO.ts
+++ backend/src/dto/userDTO/CreateUserSchemaDTO.ts
@@ -1,9 +1,10 @@
 import { z } from "zod"
+import { UserRole } from "../../types/roles"
 
 
 export const createUserSchema = z.object({
     nome: z.string().trim().min(1, "Nome e obrigatorio").max(100, "Nome deve ter no maximo 100 caracteres"),
-    email: z.email().trim().toLowerCase(),
+    email: z.email("E-mail invalido").trim().toLowerCase(),
     senha: z.string().min(6).refine((s) => /[A-Z]/.test(s), {
         error: "A senha deve conter ao menos 1 letra maiuscula"
     })
@@ -15,7 +16,8 @@ export const createUserSchema = z.object({
     })
     .refine((s) => /[0-9]/.test(s), {
         error: "A senha deve conter ao menos 1 numero"
-    })
+    }),
+    role: z.enum(UserRole)
 
 })
 
