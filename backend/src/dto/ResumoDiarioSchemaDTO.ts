7858df5e0cb00d507c878458f58e0254ce0fd9ae
princeDev <davinomoises@outlook.com>
2026-04-03 15:41:03 -0400

    feat:cria validacao resumoDiario

---
+++ backend/src/dto/ResumoDiarioSchemaDTO.ts
@@ -0,0 +1,29 @@
+import { z } from "zod"
+import { StatusResumo } from "../types/statusResumo"
+
+export const resumoDiariSchemaDTO = z.object({
+    colaboradorId: z.uuid("Colaborador invalido"),
+
+    data: z.coerce.date(),
+
+    horasTrabalhadas: z
+    .number()
+    .min(0, "Horas trabalhadas nao podem ser negativas"),
+
+    horasEsperadas: z
+    .number()
+    .positive("Horas esperadas devem ser maiores que zero"),
+
+    horasExtras: z
+    .number()
+    .min(0, "Horas extras nao podem ser negativas"),
+
+    atrasoMinutos: z
+    .number()
+    .int("Atraso deve ser um numero inteiro")
+    .min(0, "Atraso nao pode ser negativo"),
+
+    status: z.enum(StatusResumo)
+})
+
+export type ResumoDiaripSchemaDTO = z.infer<typeof resumoDiariSchemaDTO>
