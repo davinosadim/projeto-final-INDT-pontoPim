3c9f0e23acfcd7ce1ab0d564f750f83f49e1cb43
princeDev <davinomoises@outlook.com>
2026-04-06 20:21:17 -0400

    feat:cria refreshTokenService

---
+++ backend/src/services/RefreshTokenService.ts
@@ -0,0 +1,55 @@
+import type { JwtPayload } from "jsonwebtoken";
+import { appDataSource } from "../database/data-source";
+import { User } from "../entities/User";
+import { RefreshToken } from "../entities/RefreshToken";
+import { jwtConfig } from "../config/jwt.config";
+import jwt from "jsonwebtoken";
+import { AppError } from "../errors/AppError";
+import { email } from "zod";
+
+export class RefreshTokenService {
+
+    private refreshRepository = appDataSource.getRepository(RefreshToken)
+    private userRepository = appDataSource.getRepository(User)
+
+    async execute(refreshTokenJWT: string) {
+        let payload: JwtPayload
+
+        try {
+            payload = jwt.verify(
+                refreshTokenJWT,
+                jwtConfig.refresh.secret
+            ) as JwtPayload
+            
+        } catch (error) {
+            throw new AppError("Refresh token invalido")
+            
+        }
+        
+        if (payload.type !== "refresh" || !payload.jti || !payload.sub) {
+            throw new AppError("Refresh token invalido")
+        }
+
+        const session = await this.refreshRepository.findOne({
+            where: {jti: payload.jti},
+            relations: ["user"]
+        })
+
+        if(!session) {
+            throw new AppError("Sessao expirada")
+        }
+
+        const acessToken = jwt.sign({
+            sub: session.user.id,
+            email: session.user.email,
+            type: "acess"
+        }, 
+        jwtConfig.access.secret,
+        {
+            expiresIn: jwtConfig.access.expiresIn!
+        }
+    )
+
+    return acessToken
+    }
+}
