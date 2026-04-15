c6543a1d9a3f4884383c0a05ee2b2d36e661fe61
vinicius-dev-data-scientist <vinicius.dev.tech@gmail.com>
2026-04-15 17:01:46 -0300

    Atualizações: Desenvolvendo Colaborador DTO

--- backend/src/dto/colaborador/CreateColaboradorSchemaDTO.ts
+++ backend/src/dto/colaborador/CreateColaboradorSchemaDTO.ts
@@ -1,16 +1,21 @@
 import { z } from "zod"
 import { Turno } from "../../types/turno"
 import { converterHorarioParaMinutos } from "../../utils/time"
+import { Cargos } from "../../types/cargos"
+import { Setores } from "../../types/setores"
 
 const horarioRegex = /^([01]\d|2[0-3]):([0-5]\d)$/
+const turnoValues = Object.values(Turno) as string[]
+const turnoEnum = z.enum(turnoValues)
+const cargoValues = Object.values(Cargos) as string[]
+const setorValues = Object.values(Setores) as string[]
 
 export const createColaboradorSchema = z.object({
     nome: z.string().trim().min(1, "O nome e obrigatorio").max(100, "Nome deve ter no maximo 100 caracteres"),
-    matricula: z.string().trim().min(1, "Matricula e obrigatoria").max(5, "Matricula deve ter no maximo 5 numeros"),
-    cargo: z.string().trim().min(1, "Cargo e obrigatorio").max(100, "Cargo deve ter no maximo 100 caracteres"),
-    setor: z.string().min(1, "Nome e obrigatorio"),
-    setorId: z.uuid("Setor invalido"),
-    turno: z.enum(Turno),
+    matricula: z.string().trim().min(1, "Matricula e obrigatoria").max(6, "Matricula deve ter no maximo 6 caracteres"),
+    cargo: cargoValues.length > 0 ? z.enum(cargoValues) : z.string(),
+    setor: setorValues.length > 0 ? z.enum(setorValues) : z.string().min(1, "Nome e obrigatorio"),
+    turno: turnoValues.length > 0 ? turnoEnum : z.string().min(1, "Turno e obrigatorio"),
 
     cargaHorariaDia: z.coerce.number()
     .positive("Carga horaria diaria deve ser maior que zero")
