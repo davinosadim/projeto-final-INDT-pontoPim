84a9e2d434c9ac974936a60ffa285614da66c096
princeDev <davinomoises@outlook.com>
2026-04-15 21:27:05 -0400

    feat: cria rota registro ponto

---
+++ backend/src/routes/registroPontoRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { RegistroPontoController } from "../controllers/RegistroPontoController";
+import { validate } from "../middlewares/validateBody";
+import { createRegistroPontoSchemaDTO } from "../dto/registro-ponto.ts/CreateRegistroPontoSchemaDTO";
+
+const registroPontoRoutes = Router()
+const controller = new RegistroPontoController()
+
+registroPontoRoutes.post("/ponto", validate(createRegistroPontoSchemaDTO), controller.create.bind(controller))
+
+export default registroPontoRoutes
